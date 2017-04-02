c890fd62db4e60005a5c103819c0f0c7eb98d345
Shoodey <shoodey@gmail.com>
2017-04-02 06:07:15 +0100

    config & database

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Laravel',
+    'name' => 'AUI - Exam planner',
 
     /*
     |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://aui-ep.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -103,7 +103,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => 'base64:9MG7GcU5X33FyDHOMlSybr16wRGmJiAhE0Lz1DrNjoM=',
 
     'cipher' => 'AES-256-CBC',
 
